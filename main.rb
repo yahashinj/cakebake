34ce0b22204e3593c2f01c4d566bd6e0365ffb48
yahashinj <yahashinj@s.dcsdk12.org>
2015-12-04 10:06:17 -0700

    Added Movement to chef

---
+++ main.rb
@@ -0,0 +1,31 @@
+require "gosu"
+require_relative "player"
+
+class Main < Gosu::Window
+	
+	def initialize
+		super 800, 800
+		self.caption = "Cake Baking Simulator 2015"
+		@background = Gosu::Image.new("background.png")
+		@player_anim = Gosu::Image::load_tiles("chef.png", 114, 216)
+		@player = Player.new(@player_anim)
+	end
+
+	def draw
+		@background.draw(0, 0, 0)
+		@player.draw_anim
+	end
+
+	def update
+		@player.move_left if Gosu::button_down? Gosu::KbLeft
+		@player.move_right if Gosu::button_down? Gosu::KbRight
+	end
+
+	def button_down(id)
+		close if id == Gosu::KbEscape
+	end
+
+end
+
+window = Main.new 
+window.show
---
+++ player.rb
@@ -0,0 +1,43 @@
+require "gosu"
+
+class Player
+
+	attr_reader :score
+	
+	def initialize animation
+		@animation = animation
+		@score = 0
+		@x = 300
+		@y = 500
+		@anim = 0
+		@angle = 90
+	end
+
+	def draw
+		img = @animation[0]
+		img.draw(@x, @y, 1)
+	end
+
+	def draw_anim
+		img = @animation[@anim / 5 % @animation.size]
+		img.draw(@x, @y, 1)
+	end
+
+	def move_left
+		@x -= 5
+		@anim += 1
+
+		@x = 790 if @x <= -100
+	end
+
+	def move_right
+		@x += 5
+		@anim += 1
+		@x = -90 if @x >= 810
+	end
+
+	def warp(x, y)
+		@x, @y = x,y
+	end
+
+end
